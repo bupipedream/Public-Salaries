bec41d40cc667fcf88afda7de1f3a96eb2d08f97
JibronHarris <Jibron.Harris@Gmail.com>
2013-01-21 22:00:37 -0500

    #2 added bootstrap

--- application/views/main.php
+++ application/views/main.php
@@ -3,6 +3,8 @@
 <head>
   <meta charset="utf-8">
   <title>Main View</title>
+  <!-- Bootstrap --> 
+  <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
 </head>
 <body>
     <h1>This is the main view for this project <h1>
