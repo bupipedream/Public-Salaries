8ae416d4ca899685c53b52ad2e3058bdaa454cb1
Timothy Boehm <timothy.j.boehm@gmail.com>
2013-01-21 20:42:51 -0500

    removed default controller + view, added new ones and set them to default.  this should fix issue #1

---
+++ application/views/main.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Main View</title>
+</head>
+<body>
+    <h1>This is the main view for this project <h1>
+</body>
+</html>
