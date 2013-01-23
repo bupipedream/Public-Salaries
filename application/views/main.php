0af5a3b5131407f993d70a14ab245900a9c91184
Daniel O'Connor <daniel@danoc.me>
2013-01-22 23:19:12 -0500

    demo commit

--- application/views/main.php
+++ application/views/main.php
@@ -4,9 +4,157 @@
   <meta charset="utf-8">
   <title>Main View</title>
   <!-- Bootstrap --> 
-  <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+  <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+  <link href="css/main.css" rel="stylesheet" media="screen">
 </head>
 <body>
-    <h1>This is the main view for this project <h1>
+	<div class="container">
+		<header>
+			<h1>Faculty Salaries</h1>
+			<ul class="nav nav-pills">
+				<li class="active">
+					<a href="#">2012</a>
+				</li>
+				<li><a href="#">2011</a></li>
+			</ul>
+		</header>
+
+		<form class="form-search">
+			<input type="text" class="input-medium search-query">
+ 			<button type="submit" class="btn">Search</button>
+		</form>
+
+		<div class="special-container">
+			<p class="special">This is a sample paragraph. This is a sample paragraph. This is a sample paragraph. This is a sample paragraph. This is a sample paragraph. This is a sample paragraph. This is a sample paragraph. This is a sample paragraph. This is a sample paragraph. This is a sample paragraph. This is a sample paragraph. This is a sample paragraph. </p>
+		</div>
+		<table class="table">
+			<tr>
+				<th>Last Name</th>
+				<th>First Name</th>
+				<th>Title</th>
+				<th>Salary</th>
+			</tr>
+			<tr>
+				<td>Stenger</td>
+				<td>Harvey</td>
+				<td>President</td>
+				<td>$385,000</td>
+			</tr>
+			<tr>
+				<td>Stenger</td>
+				<td>Harvey</td>
+				<td>President</td>
+				<td>$385,000</td>
+			</tr>
+			<tr>
+				<td>Stenger</td>
+				<td>Harvey</td>
+				<td>President</td>
+				<td>$385,000</td>
+			</tr>
+			<tr>
+				<td>Stenger</td>
+				<td>Harvey</td>
+				<td>President</td>
+				<td>$385,000</td>
+			</tr>
+			<tr>
+				<td>Stenger</td>
+				<td>Harvey</td>
+				<td>President</td>
+				<td>$385,000</td>
+			</tr>
+			<tr>
+				<td>Stenger</td>
+				<td>Harvey</td>
+				<td>President</td>
+				<td>$385,000</td>
+			</tr>
+			<tr>
+				<td>Stenger</td>
+				<td>Harvey</td>
+				<td>President</td>
+				<td>$385,000</td>
+			</tr>
+			<tr>
+				<td>Stenger</td>
+				<td>Harvey</td>
+				<td>President</td>
+				<td>$385,000</td>
+			</tr>
+			<tr>
+				<td>Stenger</td>
+				<td>Harvey</td>
+				<td>President</td>
+				<td>$385,000</td>
+			</tr>
+			<tr>
+				<td>Stenger</td>
+				<td>Harvey</td>
+				<td>President</td>
+				<td>$385,000</td>
+			</tr>
+			<tr>
+				<td>Stenger</td>
+				<td>Harvey</td>
+				<td>President</td>
+				<td>$385,000</td>
+			</tr>
+			<tr>
+				<td>Stenger</td>
+				<td>Harvey</td>
+				<td>President</td>
+				<td>$385,000</td>
+			</tr>
+			<tr>
+				<td>Stenger</td>
+				<td>Harvey</td>
+				<td>President</td>
+				<td>$385,000</td>
+			</tr>
+			<tr>
+				<td>Stenger</td>
+				<td>Harvey</td>
+				<td>President</td>
+				<td>$385,000</td>
+			</tr>
+			<tr>
+				<td>Stenger</td>
+				<td>Harvey</td>
+				<td>President</td>
+				<td>$385,000</td>
+			</tr>
+			<tr>
+				<td>Stenger</td>
+				<td>Harvey</td>
+				<td>President</td>
+				<td>$385,000</td>
+			</tr>
+			<tr>
+				<td>Stenger</td>
+				<td>Harvey</td>
+				<td>President</td>
+				<td>$385,000</td>
+			</tr>
+			<tr>
+				<td>Stenger</td>
+				<td>Harvey</td>
+				<td>President</td>
+				<td>$385,000</td>
+			</tr>
+		</table>
+
+		<div class="pagination">
+		  <ul>
+		    <li><a href="#">Prev</a></li>
+		    <li><a href="#">1</a></li>
+		    <li><a href="#">2</a></li>
+		    <li><a href="#">3</a></li>
+		    <li><a href="#">4</a></li>
+		    <li><a href="#">Next</a></li>
+		  </ul>
+		</div>
+
+	</div>
 </body>
 </html>
